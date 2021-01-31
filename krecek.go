8214d6742e99231e1f1b02304f98d42d3e5e867b
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-01-31 18:27:54 +0100

    snad final program

--- krecek.go
+++ krecek.go
@@ -12,7 +12,7 @@ const (
 	RUN
 	LATE
 )
-const (
+const ( // levá a pravá hemisféra, index v poli before
 	L = 0
 	R = 1
 )
@@ -21,13 +21,15 @@ type edge struct { // cesta
 	where int
 	time  int
 }
+
 type place struct {
-	there [2]bool
+	there bool
 	when  int
 }
-type Wmaze struct {
+
+type Wmaze struct { // bludiště + seznam kde už křeček byl
 	maze   [][]edge
-	before []place
+	before [][2]place
 	food   int
 }
 type retStat struct {
@@ -35,88 +37,89 @@ type retStat struct {
 	time int
 }
 
-func solve(work Wmaze, decide bool, time, start int) (end, t int) {
-	// jídlo
+func solve(work Wmaze, decide bool, time, start int) (int, int) {
+	// nají se?
 	if start == work.food {
 		return FOOD, time
 	}
-	// už jsem tu byl...
+	// už tu byl...
 	var who int
 	if decide == LEFT {
 		who = L
 	} else {
 		who = R
 	}
-	if work.before[start].there[who] {
+	if work.before[start][who].there {
 		return RUN, -1
-	}
-	if work.before[start].when != 0 && time >= work.before[start].when {
+	} else if work.before[start][who].when > 0 && time >= work.before[start][who].when { // byl tu v jiné větvi dřív
 		return LATE, -1
-	} else {
-		work.before[start].when = time
 	}
-	work.before[start].there[who] = true
+	work.before[start][who].when = time
+	work.before[start][who].there = true // byl jsem tu
 	// ponoření
 	var ret []retStat
 	ret = make([]retStat, len(work.maze[start]))
-	for i, next := range work.maze[start] {
+	for i, next := range work.maze[start] { // projdi všechny cesty co následují
 		tS, tT := solve(work, !decide, time+next.time, next.where)
 		ret[i] = retStat{tS, tT}
 	}
-	work.before[start].there[who] = false
-	for _, r := range ret {
-		if decide == LEFT {
-			var end bool
-			var max int
+	work.before[start][who].there = false // uvolnění pro průchody z jiných křižovatek
+	var max, min int
+	var run, end bool
+	if decide == LEFT { // LEFT
+		for _, r := range ret {
 			switch r.end {
-			case RUN:
+			case RUN: // můžu běžet maraton
 				return RUN, -1
-			case END:
+			case END: // nebo se uvěznit
 				end = true
-			case FOOD:
-				if r.time > max {
+			case FOOD: // či najdu jídlo
+				if r.time > max { // před jídlem chci běžet co nejdéle
 					max = r.time
 				}
 			}
-			if end {
-				return END, -1
-			} else {
-				return FOOD, max
-			}
-		} else { // RIGHT
-			var min int
-			var run, end bool
+		}
+		if end { // nejde maraton, tak se křeček uvězní
+			return END, -1
+		} else if max != 0 { // když musí, tak jde k jídlu
+			return FOOD, max
+		} else {
+			return LATE, -1
+		}
+	} else { // RIGHT
+		for _, r := range ret {
 			switch r.end {
-			case RUN:
+			case RUN: // běh
 				run = true
-			case END:
+			case END: // vězení
 				end = true
-			case FOOD:
-				if min == 0 || min > r.time {
+			case FOOD: // jídlo
+				if min == 0 || min > r.time { // chci nejkratší(časově) cestu
 					min = r.time
 				}
 			}
-			if min > 0 {
-				return FOOD, min
-			} else if run {
-				return RUN, -1
-			} else if end {
-				return END, -1
-			}
+		}
+		if min > 0 { // k jídlu
+			return FOOD, min
+		} else if run { // běž
+			return RUN, -1
+		} else if end { // vězení
+			return END, -1
+		} else {
+			return LATE, -1
 		}
 	}
-	return
 }
 
 func main() {
 	var n, m, s, t int
 	var maze [][]edge
-	var before []place
+	var before [][2]place
 	var work Wmaze
 
 	fmt.Scan(&n, &m, &s, &t) // načtení dat
 	maze = make([][]edge, n)
-	before = make([]place, n)
+	before = make([][2]place, n)
 	work = Wmaze{maze, before, t}
 	for i := range maze { // inicializace bludiště
 		maze[i] = make([]edge, 0)
