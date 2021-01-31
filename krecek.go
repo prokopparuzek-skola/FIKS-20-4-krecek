e42b0d7b225503a7a30b22aa06328aade1e0f655
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2021-01-31 13:31:07 +0100

    vyhodnoceni solve

--- krecek.go
+++ krecek.go
@@ -23,11 +23,63 @@ type place struct {
 	RWhere int
 }
 type Wmaze struct {
-	maze   *[][]edge
-	before *[]place
+	maze   [][]edge
+	before []place
+	food   int
+}
+type retStat struct {
+	end  int
+	time int
 }
 
-func solve(work *Wmaze, decide bool, time int) (end, t int) {
+func solve(work Wmaze, decide bool, time, start int) (end, t int) {
+	var ret []retStat
+	ret = make([]retStat, len(work.maze[start]))
+	for i, next := range work.maze[start] {
+		tS, tT := solve(work, !decide, time+next.time, next.where)
+		ret[i] = retStat{tS, tT}
+	}
+	for _, r := range ret {
+		if decide == LEFT {
+			var end bool
+			var max int
+			switch r.end {
+			case RUN:
+				return RUN, -1
+			case END:
+				end = true
+			case FOOD:
+				if r.time > max {
+					max = r.time
+				}
+			}
+			if end {
+				return END, -1
+			} else {
+				return FOOD, max
+			}
+		} else { // RIGHT
+			var min int
+			var run, end bool
+			switch r.end {
+			case RUN:
+				run = true
+			case END:
+				end = true
+			case FOOD:
+				if min == 0 || min > r.time {
+					min = r.time
+				}
+			}
+			if min > 0 {
+				return FOOD, min
+			} else if run {
+				return RUN, -1
+			} else if end {
+				return END, -1
+			}
+		}
+	}
 	return
 }
 
@@ -40,7 +92,7 @@ func main() {
 	fmt.Scan(&n, &m, &s, &t) // načtení dat
 	maze = make([][]edge, n)
 	before = make([]place, n)
-	work = Wmaze{&maze, &before}
+	work = Wmaze{maze, before, t}
 	for i := range maze { // inicializace bludiště
 		maze[i] = make([]edge, 0)
 	}
@@ -49,7 +101,7 @@ func main() {
 		fmt.Scan(&a, &b, &w)
 		maze[a] = append(maze[a], edge{b, w})
 	}
-	end, time := solve(&work, LEFT, 0)
+	end, time := solve(work, LEFT, 0, s)
 	switch end {
 	case FOOD:
 		fmt.Println(time)
